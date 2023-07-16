83043bc7437d1b934db58f983fd08af16d6f0a60
Rakesh Sharma <rakesh.sharma.ee19@gmail.com>
2023-07-16 21:43:33 +0530

    Create arrays_ds.py

---
+++ problem_solving/arrays/arrays_ds.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'reverseArray' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def reverseArray(a):
+    # Write your code here
+    first=0
+    last=len(a)-1
+    while first<last:
+        a[first],a[last]=a[last],a[first]
+        first+=1
+        last=last-1
+    return a
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    res = reverseArray(arr)
+
+    fptr.write(' '.join(map(str, res)))
+    fptr.write('\n')
+
+    fptr.close()
