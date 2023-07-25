353fc3a6e768baea9856e3ccf430fdc05627a391
Rakesh Sharma <rakesh.sharma.ee19@gmail.com>
2023-07-25 11:14:00 +0530

    Create sherlock_and_array.py

---
+++ problem_solving/search/sherlock_and_array.py
@@ -0,0 +1,23 @@
+def balancedSums(arr):
+    l=0
+    r=len(arr)-1
+    res='NO'
+    if len(arr)==1:
+        res='YES'
+    else:
+        while l<r:
+            mid = (r+l)//2
+            if sum(arr[:mid])==sum(arr[mid+1:]):
+                res='YES'
+                break
+            if sum(arr[:mid])>sum(arr[mid+1:]):
+                if mid==r:
+                    r-=1
+                else:
+                    r=mid
+            else:
+                if l==mid:
+                    l+=1
+                else:
+                    l=mid
+    return res
