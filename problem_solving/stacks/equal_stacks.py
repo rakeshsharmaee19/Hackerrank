7b75d1b6bccefbe1d82f88a7924574780a9c94c7
Rakesh Sharma <rakesh.sharma.ee19@gmail.com>
2023-08-13 21:26:12 +0530

    Update and rename games_of_two_stacks.py to equal_stacks.py

---
+++ problem_solving/stacks/equal_stacks.py
@@ -0,0 +1,77 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'equalStacks' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY h1
+#  2. INTEGER_ARRAY h2
+#  3. INTEGER_ARRAY h3
+#
+
+def equalStacks(h1, h2, h3):
+    # Write your code here
+    if (sum(h1)==sum(h2)) and (sum(h2)==sum(h3)):
+        return sum(h1)
+    else:
+        h = min(sum(h1), sum(h2), sum(h3))
+        sum_h1 = sum(h1)
+        sum_h2 = sum(h2)
+        sum_h3 = sum(h3)
+        while not(sum_h1==sum_h2 and sum_h2==sum_h3):
+            h = min(sum_h1, sum_h2, sum_h3)
+            if sum_h1==h:
+                if sum_h2 == h:
+                    sum_h3-=h3.pop(0)
+                elif sum_h3 == h:
+                    sum_h2-=h2.pop(0)
+                else:
+                    sum_h2-=h2.pop(0)
+                    sum_h3-=h3.pop(0)
+            elif sum_h2==h:
+                if sum_h1 == h:
+                    sum_h3-=h3.pop(0)
+                elif sum_h3 == h:
+                    sum_h1-=h1.pop(0)
+                else:
+                    sum_h1-=h1.pop(0)
+                    sum_h3-=h3.pop(0)
+            else:
+                if sum_h2 == h:
+                    sum_h1-=h1.pop(0)
+                elif sum_h1 == h:
+                    sum_h2-=h2.pop(0)
+                else:
+                    sum_h2-=h2.pop(0)
+                    sum_h1-=h1.pop(0)
+        return sum_h1
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n1 = int(first_multiple_input[0])
+
+    n2 = int(first_multiple_input[1])
+
+    n3 = int(first_multiple_input[2])
+
+    h1 = list(map(int, input().rstrip().split()))
+
+    h2 = list(map(int, input().rstrip().split()))
+
+    h3 = list(map(int, input().rstrip().split()))
+
+    result = equalStacks(h1, h2, h3)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
